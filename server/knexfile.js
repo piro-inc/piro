1fe3ac2f145f75b3e32404e20c735812205f337a
george francis <georgefrancis105@gmail.com>
2016-07-28 21:11:05 +1200

    Change dev db to postgres, remove key references key from comments and game tables

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,11 +1,21 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 
