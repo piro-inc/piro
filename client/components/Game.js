1b40b5eeca12c4e0d67628b2e9c85726a6ae45d1
george francis <georgefrancis105@gmail.com>
2016-07-30 11:55:27 +1200

    Complete container functions for Game component

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,20 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { toJS } from 'immutable'
 
-class Game extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      // state goes here
-    }
-  }
+//import { fetchGameInfo } from '../redux/gameActions'
 
-  render () {
-    return (
-      <div>
-        <p>wtf is going on here?</p>
-      </div>
-    )
+const Game = (props) => {
+  return (
+    <div>
+      <p>wtf is going on here?</p>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    game: state.get(game).toJS()
   }
 }
 
-export default Game
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchGameInfo: (id) => {
+      dispatch(fetchGameInfo(id))
+    }
+  }
+}
+
+const GameContainer = connect(mapStateToProps, mapDispatchToProps)(Game)
+
+export default GameContainer
