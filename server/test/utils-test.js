2366fb97e23346390d895b402a726a2537192658
Kristy Johnstone <Kristy@Kristy-Johnstones-MacBook-Pro.local>
2016-07-29 18:13:38 +1200

    Game and Comment Table tests

--- server/test/utils-test.js
+++ server/test/utils-test.js
@@ -11,7 +11,7 @@ test('Test Works', function (t) {
   t.end()
 })
 
-test('Get user from users table', function (t) {
+test('Get a user from users table', function (t) {
   knex.migrate.rollback()
     .then(() => knex.migrate.latest())
     .then(() => knex.seed.run('users'))
@@ -21,6 +21,48 @@ test('Get user from users table', function (t) {
   .then((user) => {
     console.log(user)
     t.ok(1)
+    t.end()
+  })
+  .catch((err) => {
+    t.ok(0, err)
+    t.end()
+  })
+})
+
+test('Get a comment from comments table', function (t) {
+
+  const expected = [ { id : 1, game_id: 3, comment: 'Player 1 injured' }]
+
+  knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run('comments'))
+    .then(() => {
+      return dbUtils.getOne('comments', { id: 1 })
+    })
+  .then((comment) => {
+    console.log(comment)
+    t.deepEqual(comment, expected)
+    t.end()
+  })
+  .catch((err) => {
+    t.ok(0, err)
+    t.end()
+  })
+})
+
+test('Get a game from games table', function (t) {
+
+  const expected = 'the fun team'
+
+  knex.migrate.rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => knex.seed.run('games'))
+    .then(() => {
+      return dbUtils.getOne('games', { id: 1 })
+    })
+  .then((game) => {
+    console.log(game[0].team_a_name)
+    t.deepEqual(game[0].team_a_name, expected, 'got team from games table')
     t.end()
   })
   .catch((err) => {
