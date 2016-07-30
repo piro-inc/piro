1e5c1c1d2097a610f0a98559d31cce774dd67017
danielkermode <danielkermode@hotmail.co.nz>
2016-07-30 23:41:45 +1200

    change table restrictions user

--- server/migrations/20160728181606_users.js
+++ server/migrations/20160728181606_users.js
@@ -2,8 +2,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('users', (table) => {
     table.increments('id').primary()
     table.string('username').unique().notNullable()
-    table.string('password')
-    table.string('email')
+    table.string('password').notNullable()
+    table.string('email').unique().notNullable()
   })
 }
 
