b168cbf3f55b68f2e2437dc92f353047fe4e21cb
danielkermode <danielkermode@hotmail.co.nz>
2016-07-31 01:57:44 +1200

    change error validation

--- client/components/Register.js
+++ client/components/Register.js
@@ -1,61 +1,102 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { register } from '../redux/sessionActions'
-import validator from 'email-validator'
+
+function errorExists(obj) {
+  let res = false
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === 'object') {
+      if (errorExists(obj[key])) {
+        res = true
+      }
+    } else {
+      if (key === 'error' && obj[key]) {
+        res = true
+      }
+    }
+  })
+  return res
+}
 
 class Register extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      username: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
-      registerError: ''
+      username: { value: '', error: '' },
+      email: { value: '', error: '' },
+      password: { value: '', error: '' },
+      passwordConfirm: { value: '', error: '' }
     }
   }
 
   userChange = (e) => {
-    this.setState({ username: e.target.value })
+    const toUpdate = {
+      value: e.target.value,
+      error: ''
+    }
+    this.setState({ username: toUpdate })
+    if (e.target.value.length < 4 || e.target.value.length > 20) {
+      toUpdate.error = 'Username must be between 4 and 20 characters.'
+      this.setState({ username: toUpdate })
+    }
   }
 
   emailChange = (e) => {
-    this.setState({ email: e.target.value })
+    const toUpdate = {
+      value: e.target.value,
+      error: ''
+    }
+    this.setState({ email: toUpdate })
+    if (!/.+@.+/.test(e.target.value)) {
+      toUpdate.error = 'Email isn\'t valid.'
+      this.setState({ email: toUpdate })
+    }
   }
 
   pwChange = (e) => {
-    this.setState({ password: e.target.value })
+    const toUpdate = {
+      value: e.target.value,
+      error: ''
+    }
+    this.setState({ password: toUpdate })
+    if (e.target.value.length < 5 || e.target.value.length > 30) {
+      toUpdate.error = 'Password must be between 5 and 30 characters'
+      this.setState({ password: toUpdate })
+    }
   }
 
   pwcChange = (e) => {
-    this.setState({ passwordConfirm: e.target.value })
+    const toUpdate = {
+      value: e.target.value,
+      error: ''
+    }
+    this.setState({ passwordConfirm: toUpdate })
+    if (this.state.password.value !== e.target.value) {
+      toUpdate.error = 'Passwords don\'t match.'
+      this.setState({ passwordConfirm: toUpdate })
+    }
   }
 
   register = (e) => {
-    if (this.state.password.length < 5 || this.state.password.length > 30) {
-      this.setState({ registerError: 'Password must be between 5 and 30 characters, sorry.' })
-    } else if (this.state.username.length < 5 || this.state.username.length > 20) {
-      this.setState({ registerError: 'Username must be between 5 and 20 characters, sorry.' })
-    } else if (!validator.validate(this.state.email)) {
-      this.setState({ registerError: 'Email isn\'t valid, sorry.' })
-    } else if (this.state.password !== this.state.passwordConfirm) {
-      this.setState({ registerError: 'Passwords don\'t match, sorry.' })
-    } else {
+    if(!errorExists(this.state)) {
       this.props.register(this.state.username, this.state.email, this.state.password)
     }
   }
 
   render () {
     return (
-      <form id='register-form' action='#'>
+      <div id='register-form'>
         {this.props.error && <div>{this.props.error.message}</div>}
-        {this.state.registerError && <div>{this.state.registerError}</div>}
+        {this.state.username.error && <div>{this.state.username.error}</div>}
         <input type='text' onChange={this.userChange} placeholder='Enter username' id='username' />
+        {this.state.email.error && <div>{this.state.email.error}</div>}
         <input type='text' onChange={this.emailChange} placeholder='Email' id='username' className='email' />
+        {this.state.password.error && <div>{this.state.password.error}</div>}
         <input type='password' onChange={this.pwChange} placeholder='Enter password' id='password' className='password' />
+        {this.state.passwordConfirm.error && <div>{this.state.passwordConfirm.error}</div>}
         <input type='password' onChange={this.pwcChange} placeholder='Re-enter password' id='password' className='password' />
         <button onClick={this.register} className='submit button'>Register to PIRO</button>
-      </form>
+      </div>
     )
   }
 }
