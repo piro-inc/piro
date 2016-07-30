830d7d3d9c6a2bf8c7f180cbd34c5ffbf8e9859c
rich-farr <scarletmanuka@gmail.com>
2016-07-30 17:52:39 +1200

    Containerise Register component

--- client/components/Register.js
+++ client/components/Register.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { login } from '../redux/sessionActions'
+import { register } from '../redux/sessionActions'
 
-const Register = (props) => {
-  return (
-    <form id='register-form' action='#'>
-      <input type='text' placeholder='Enter username' id='username' />
-      <input type='password' placeholder='Enter password' className='password' />
-      <input type='password' placeholder='Re-enter password' className='password' />
-      <input type='text' placeholder='Email' className='email' />
-      <button type='submit' className='submit button'>Register to PIRO</button>
-    </form>
-  )
+class Register extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      // state goes here
+    }
+  }
+
+  render () {
+    return (
+      <form id='register-form' action='#'>
+        <input type='text' placeholder='Enter username' id='username' />
+        <input type='password' placeholder='Enter password' className='password' />
+        <input type='password' placeholder='Re-enter password' className='password' />
+        <input type='text' placeholder='Email' className='email' />
+        <button type='submit' className='submit button'>Register to PIRO</button>
+      </form>
+    )
+  }
 }
 
 const mapStateToProps = f => f
