e7e10a45a9d18f3e50f724c83f3ac5e4af35c162
Apprentice <apprentice@StMac1.local>
2016-07-31 11:27:40 +1200

    Component create #39

--- client/components/Create.js
+++ client/components/Create.js
@@ -17,13 +17,12 @@ class Create extends React.Component {
       <div>
         <Navbar />
         <form id='create-form' action='#'>
-          <h1>This the create form</h1>
-          <input type='text' placeholder='Sport' id='sport' />
-          <input type='text' placeholder='Team A' id='team-a' />
-          <input type='text' placeholder='Team B' id='team-b' />
-          <input type='text' placeholder='Location' id='location' />
-          <input type='datetime' placeholder='Date and time' name='Date and time' id='game-date' />
-          <button type='submit'>Submit</button>
+          <input type='text' placeholder='CHOOSE SPORT' id='sport' classname='sport' />
+          <input type='text' placeholder='TEAM 1' id='team-one' classname='team-name' />
+          <input type='text' placeholder='TEAM 2' id='team-two'classname='team-name' />
+          <input type='text' placeholder='LOCATION' id='game-location'classname='location' />
+          <input type='datetime' placeholder='DATE & START TIME' name='Date and time' id='game-date' classname='game-date' />
+          <button type='submit'>CREATE</button>
         </form>
       </div>
     )
