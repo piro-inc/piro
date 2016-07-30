a40d188c3092fe914b013d2c8b169ab15f2b0619
Rena Goldsmith <rena.kg@gmail.com>
2016-07-30 13:50:57 +1200

    assign html to Preview

--- client/components/Preview.js
+++ client/components/Preview.js
@@ -4,14 +4,47 @@ class Preview extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      // state goes here
-    }
   }
+}
 
   render () {
     return (
-      <div>
-        This be a game preview!
+
+      <div id='preview'>
+
+        <div className='preview-header'>
+          <h4 className='sport'>Sport name</h4>
+          <h3 className='division'>Division</h3>
+          <h4 className='location'>Location</h4>
+        </div>
+
+        <div className='team-names'>
+          <h3 className='team-one'>Team one</h3>
+          <h3 className='team-two'>Team two</h3>
+        </div>
+
+        <div className='score-wrapper'>
+
+          <a href='#'>
+            <img src='#' className='team-logo'/>
+          </a>
+
+          <h1 className='preview-score'>Team one score</h1>
+
+          <h1 className='period'></h1>
+
+          <h1 className='preview-score'>Team two score</h1>
+
+          <a href='#'>
+            <img src='#' className='team-logo'/>
+          </a>
+
+        </div>
+
+        <div className='comments'>
+          <p className='preview-comment'>Latest comment</p>
+        </div>
+
       </div>
     )
   }
