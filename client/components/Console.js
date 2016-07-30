29206c6229e35a94e00c3e0ed9f571cee006b783
Rena Goldsmith <rena.kg@gmail.com>
2016-07-31 11:05:34 +1200

    add classNames and ids to Console

--- client/components/Console.js
+++ client/components/Console.js
@@ -23,15 +23,15 @@ class Console extends React.Component {
         <div className='console-timer'>
 
           <div className='pause'>
-            <button className='button'>PAUSE</button>
+            <button className='button' id='pause'>PAUSE</button>
           </div>
 
           <div className='start'>
-            <button className='button'>START</button>
+            <button className='button' id='start'>START</button>
           </div>
 
           <div className='stop'>
-            <button className='button'>STOP</button>
+            <button className='button' id='stop'>STOP</button>
           </div>
 
         </div>
@@ -42,11 +42,11 @@ class Console extends React.Component {
           <div className='console-teamone'>
 
             <img src='#' className='team-logo' />
-            <h1 className='console-score'>0</h1>
+            <h1 className='console-score' id='team-one-score'>0</h1>
 
             <div className='scoring-buttons'>
-              <button className='button increment'>+</button>
-              <button className='button decrement'>-</button>
+              <button className='button increment' id='increment-team-one'>+</button>
+              <button className='button decrement' id='decrement-team-two'>-</button>
             </div>
 
           </div>
@@ -57,8 +57,8 @@ class Console extends React.Component {
             <h1 className='console-score'>0</h1>
 
             <div className='scoring-buttons'>
-              <button className='button increment'>+</button>
-              <button className='button decrement'>-</button>
+              <button className='button increment' id='increment-team-two'>+</button>
+              <button className='button decrement' id='decrement-team-two'>-</button>
             </div>
 
           </div>
@@ -67,14 +67,14 @@ class Console extends React.Component {
 
         <h3 className='console-headers'>ADD COMMENT</h3>
         <div className='add-comment'>
-          <input type='text' className='console-comment' />
-          <button type='submit' className='submit button'>+ Submit</button>
+          <input type='text' className='console-comment' id='add-comment'/>
+          <button type='submit' className='submit button' id='submit-comment'>+ Submit</button>
         </div>
 
         <h3 className='console-headers'>ADD COMMENT</h3>
         <div className='edit-comment'>
-          <input type='text' className='console-comment' />
-          <button className='submit button'>Edit</button>
+          <input type='text' className='console-comment' id='recent-comment' />
+          <button className='submit button' id='edit-comment'>Edit</button>
         </div>
       </div>
     )
