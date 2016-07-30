456bd16a2b85b0439a7a98ae3a6e5c0cef7d2538
danielkermode <danielkermode@hotmail.co.nz>
2016-07-30 18:15:18 +1200

    login change starting

--- client/components/Login.js
+++ client/components/Login.js
@@ -2,14 +2,36 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { login } from '../redux/sessionActions'
 
-const Login = (props) => {
-  return (
-    <form id='login-form' action='#'>
-      <input type='text' placeholder='Enter username' id='username' />
-      <input type='password' placeholder='Enter password' id='password' />
-      <button type='submit' className='submit button'>Log In</button>
-    </form>
-  )
+class Login extends React.component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  userChange = (e) => {
+    this.setState({ username: e.target.value })
+  }
+
+  pwChange = (e) => {
+    this.setState({ password: e.target.value })
+  }
+
+  login = () => {
+    this.props.login(this.state.username, this.state.password)
+  }
+
+  render () {
+    return (
+      <form id='login-form' action='#'>
+        <input type='text' onChange={userChange} placeholder='Enter username' id='username' />
+        <input type='password' onChange={pwChange} placeholder='Enter password' id='password' />
+        <button type='submit' onClick={this.login} className='submit button'>Log In</button>
+      </form>
+    )
+  }
 }
 
 const mapStateToProps = f => f
