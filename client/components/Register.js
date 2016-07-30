24017ce0308b630b764911199cee0c50bd1fcd7b
rich-farr <scarletmanuka@gmail.com>
2016-07-30 17:39:40 +1200

    Containerise Register component

--- client/components/Register.js
+++ client/components/Register.js
@@ -1,24 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { login } from '../redux/sessionActions'
 
-class Register extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      // state goes here
+const Register = (props) => {
+  return (
+    <form id='register-form' action='#'>
+      <input type='text' placeholder='Enter username' id='username' />
+      <input type='password' placeholder='Enter password' className='password' />
+      <input type='password' placeholder='Re-enter password' className='password' />
+      <input type='text' placeholder='Email' className='email' />
+      <button type='submit' className='submit button'>Register to PIRO</button>
+    </form>
+  )
+}
+
+const mapStateToProps = f => f
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    register: (username, email, password) => {
+      dispatch(register(username, email, password))
     }
-  }
-
-  render () {
-    return (
-      <form id='register-form' action='#'>
-        <input type='text' placeholder='Enter username' id='username' />
-        <input type='password' placeholder='Enter password' className='password' />
-        <input type='password' placeholder='Re-enter password' className='password' />
-        <input type='text' placeholder='Email' className='email' />
-        <button type='submit' className='submit button'>Register to PIRO</button>
-      </form>
-    )
   }
 }
 
-export default Register
+const RegisterContainer = connect(mapStateToProps, mapDispatchToProps)(Register)
+
+export default RegisterContainer
