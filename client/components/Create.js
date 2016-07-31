a41894b4cfa0bc5789ec25e818425668b495b139
danielkermode <danielkermode@hotmail.co.nz>
2016-07-31 16:20:27 +1200

    linting can be frustrating :)

--- client/components/Create.js
+++ client/components/Create.js
@@ -15,15 +15,15 @@ const sports = [
   'football'
 ]
 
-function readCookie(name) {
-	const nameEQ = name + "="
-	const ca = document.cookie.split(';')
-	for(let i=0;i < ca.length;i++) {
-		let c = ca[i]
-		while (c.charAt(0)==' ') c = c.substring(1,c.length)
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length)
-	}
-	return null
+function readCookie (name) {
+  const nameEQ = name + '='
+  const ca = document.cookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length)
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length)
+  }
+  return null
 }
 
 class Create extends React.Component {
