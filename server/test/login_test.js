6ccfedb1b389fbca2be55ff9be073e578468db4f
george francis <georgefrancis105@gmail.com>
2016-07-31 16:30:12 +1200

    Remove login test

--- server/test/login_test.js
+++ server/test/login_test.js
@@ -6,16 +6,16 @@ test('Login test segment', function (t) {
   t.end()
 })
 
-test('We can get blocked by the API', function (t) {
-  const expected = 403
+// test('We can get blocked by the API', function (t) {
+//   const expected = 403
 
-  request
-    .get('http://localhost:3000/api/users/2')
-    .end(function (err, res) {
-      t.deepEqual(err.status, expected, 'We are blocked from accessing users')
-      t.end()
-    })
-})
+//   request
+//     .get('http://localhost:3000/api/users/2')
+//     .end(function (err, res) {
+//       t.deepEqual(err.status, expected, 'We are blocked from accessing users')
+//       t.end()
+//     })
+// })
 
 // test('Access login', function (t) {
 //   request
