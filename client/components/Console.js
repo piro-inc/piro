49d5673c665e9d7a0c26bd5f3333ba8ad04e1a84
rich-farr <scarletmanuka@gmail.com>
2016-07-31 21:16:54 +1200

    Begin styling Console page

--- client/components/Console.js
+++ client/components/Console.js
@@ -32,61 +32,68 @@ class Console extends React.Component {
           <Navbar />
         </div>
 
-        <h4 className='console-title'>Game {} [Sport]</h4>
+        <div id='content-wrapper'>
 
-        <h3 className='console-headers'>TIMER</h3>
-        <div className='console-timer-wrapper'>
-          <div className='pause'>
-            <button className='button' id='pause'>PAUSE</button>
-          </div>
+          <h4 className='console-title'>Game {} [Sport]</h4>
 
-          <div className='start'>
-            <button className='button' id='start'>START</button>
-          </div>
+          <h3 className='console-headers'>TIMER</h3>
 
-          <div className='stop'>
-            <button className='button' id='stop'>STOP</button>
-          </div>
-        </div>
-
-        <h3 className='console-headers'>SCORE TEAMS</h3>
-        <div className='console-scores-wrapper'>
-
-          <div className='console-teamone'>
-            <img src='#' className='team-logo' />
-            <h1 className='console-score' id='team-one-score'>0</h1>
-
-            <div className='scoring-buttons'>
-              <button className='button increment' id='increment-team-one'>+</button>
-              <button className='button decrement' id='decrement-team-two'>-</button>
+          <div className='console-timer-wrapper'>
+            <div className='pause'>
+              <button className='button' id='pause'>PAUSE</button>
             </div>
 
-          </div>
-
-          <div className='console-teamtwo'>
-
-            <img src='#' className='team-logo' />
-            <h1 className='console-score'>0</h1>
-
-            <div className='scoring-buttons'>
-              <button className='button increment' id='increment-team-two'>+</button>
-              <button className='button decrement' id='decrement-team-two'>-</button>
+            <div className='start'>
+              <button className='button' id='start'>START</button>
             </div>
 
+            <div className='stop'>
+              <button className='button' id='stop'>STOP</button>
+            </div>
           </div>
-        </div>
 
-        <h3 className='console-headers'>ADD COMMENT</h3>
-        <div className='add-comment-wrapper'>
-          <input type='text' className='console-comment' id='add-comment' />
-          <button type='submit' className='submit button' id='submit-comment'>+ Submit</button>
-        </div>
+          <h3 className='console-headers'>SCORE TEAMS</h3>
 
-        <h3 className='console-headers'>EDIT COMMENT</h3>
-        <div className='edit-comment-wrapper'>
-          <input type='text' className='console-comment' id='recent-comment' />
-          <button className='edit button' id='edit-comment'>Edit</button>
-          <button type='submit' className='submit button' id='submit-edit'>Change</button>
+          <div className='console-scores-wrapper'>
+
+            <div className='console-teamone'>
+              <img src='#' className='team-logo' />
+              <h1 className='console-score' id='team-one-score'>0</h1>
+
+              <div className='scoring-buttons'>
+                <button className='button increment' id='increment-team-one'>+</button>
+                <button className='button decrement' id='decrement-team-two'>-</button>
+              </div>
+
+            </div>
+
+            <div className='console-teamtwo'>
+
+              <img src='#' className='team-logo' />
+              <h1 className='console-score'>0</h1>
+
+              <div className='scoring-buttons'>
+                <button className='button increment' id='increment-team-two'>+</button>
+                <button className='button decrement' id='decrement-team-two'>-</button>
+              </div>
+
+            </div>
+          </div>
+
+          <h3 className='console-headers'>ADD COMMENT</h3>
+
+          <div className='add-comment-wrapper'>
+            <input type='text' className='console-comment' id='add-comment' />
+            <button type='submit' className='submit button' id='submit-comment'>+ Submit</button>
+          </div>
+
+          <h3 className='console-headers'>EDIT COMMENT</h3>
+
+          <div className='edit-comment-wrapper'>
+            <input type='text' className='console-comment' id='recent-comment' />
+            <button className='edit button' id='edit-comment'>Edit</button>
+            <button type='submit' className='submit button' id='submit-edit'>Change</button>
+          </div>
         </div>
       </div>
     )
