14a736f20b398a841c2fb93ca59112f546efb21f
danielkermode <danielkermode@hotmail.co.nz>
2016-07-31 13:49:39 +1200

    get comments from route and render then

--- client/components/Game.js
+++ client/components/Game.js
@@ -17,8 +17,7 @@ class Game extends React.Component {
   }
 
   render () {
-    console.log(this.props)
-    const game = this.props.game
+    const currentGame = this.props.game
     return (
       <div id='game-wrapper'>
 
@@ -27,10 +26,10 @@ class Game extends React.Component {
         </div>
 
         <div id='game-header'>
-          <h2 className='sport-name'>{game.sport_name}</h2>
+          <h2 className='sport-name'>{currentGame.game && currentGame.game.sport_name}</h2>
           {/* <h3 className='division'>Division</h3>  No division in database yet*/}
-          <h3 className='date-time'>{game.date_time}</h3>
-          <h3 className='match-location'>{game.location}</h3>
+          <h3 className='date-time'>{currentGame.game && currentGame.game.date_time}</h3>
+          <h3 className='match-location'>{currentGame.game && currentGame.game.location}</h3>
         </div>
 
         <div className='score-wrapper'>
@@ -40,27 +39,18 @@ class Game extends React.Component {
           <a href='#'>
             <img src='#' className='team-logo' />
           </a> No logos in database yet*/}
-          <h2 className='team-one'>{game.team_a_name}</h2>
-          <h2 className='team-two'>{game.team_b_name}</h2>
+          <h2 className='team-one'>{currentGame.game && currentGame.game.team_a_name}</h2>
+          <h2 className='team-two'>{currentGame.game && currentGame.game.team_b_name}</h2>
 
-          <h1 className='game-score'>{game.team_a_score}</h1>
-          <h1 className='game-score'>{game.team_b_score}</h1>
+          <h1 className='game-score'>{currentGame.game && currentGame.game.team_a_score}</h1>
+          <h1 className='game-score'>{currentGame.game && currentGame.game.team_b_score}</h1>
 
         </div>
 
         <div className='comments'>
-          <p className='game-comment'>Latest comment</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
-          <p className='comment-history'>Comment history</p>
+        {currentGame.comments && currentGame.comments.map(obj => {
+          return <div className='comment-history'>{obj.comment}</div>
+        })}
         </div>
       </div>
     )
