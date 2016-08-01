19601b66dbb7f23177a9fd555f4d8dd88b94bedb
danielkermode <danielkermode@hotmail.co.nz>
2016-08-01 14:42:30 +1200

    database url config var

--- server/knexfile.js
+++ server/knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 
   staging: {
     client: 'postgresql',
-    connection: process.env.HEROKU_POSTGRESQL_COLOR_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -33,7 +33,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.HEROKU_POSTGRESQL_COLOR_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
