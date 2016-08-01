10a197e4fd7c316c5a5664ca1c1040042daba834
george francis <georgefrancis105@gmail.com>
2016-08-02 11:42:32 +1200

    Make following_join table migration

---
+++ server/migrations/20160802110848_following-join.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('following_join', (table) => {
+    table.integer('id').primary()
+    table.integer('user_id').references('id').inTable('users')
+    table.integer('game_id').references('id').inTable('games')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('following_join')
+};
