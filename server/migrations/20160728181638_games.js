3bcbe62a16b1cd60c4181bdfc9199c259f3055c2
danielkermode <danielkermode@hotmail.co.nz>
2016-08-02 11:43:09 +1200

    add columns to games table

--- server/migrations/20160728181638_games.js
+++ server/migrations/20160728181638_games.js
@@ -1,15 +1,18 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('games', (table) => {
     table.increments('id').primary()
-    table.integer('user_id')
-    table.timestamp('date_time')
-    table.string('location')
-    table.string('team_a_name')
-    table.string('team_b_name')
-    table.boolean('is_complete')
-    table.integer('team_a_score')
-    table.integer('team_b_score')
-    table.string('sport_name')
+    table.integer('user_id').notNullable()
+    table.timestamp('date_time').notNullable()
+    table.string('location').notNullable()
+    table.string('team_a_name').notNullable()
+    table.string('team_b_name').notNullable()
+    table.boolean('is_complete').notNullable()
+    table.integer('team_a_score').notNullable()
+    table.integer('team_b_score').notNullable()
+    table.string('sport_name').notNullable()
+    table.time('time_elapsed').notNullable()
+    table.timestamps(true, true)
+    table.boolean('is_running').notNullable()
   })
 }
 
--- server/seeds/games.js
+++ server/seeds/games.js
@@ -5,9 +5,45 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('games').insert({user_id: 1, date_time: 990108, location: 'EDA', team_a_name: 'the fun team', team_b_name: 'the other guys', is_complete: false, team_a_score: 1, team_b_score: 5, sport_name: 'ninja'}),
-        knex('games').insert({user_id: 1, date_time: 990108, location: 'Vivian St', team_a_name: 'the ok team', team_b_name: 'the other team', is_complete: false, team_a_score: 4, team_b_score: 2, sport_name: 'coding'}),
-        knex('games').insert({user_id: 2, date_time: 990108, location: 'Cuba St', team_a_name: 'the great team', team_b_name: 'the others', is_complete: false, team_a_score: 1, team_b_score: 1, sport_name: 'hackysack'})
+        knex('games').insert({
+          user_id: 1,
+          date_time: 990108,
+          location: 'EDA',
+          team_a_name: 'the fun team',
+          team_b_name: 'the other guys',
+          is_complete: false,
+          team_a_score: 1,
+          team_b_score: 5,
+          sport_name: 'ninja',
+          time_elapsed: '00:00:00',
+          is_running: false
+        }),
+        knex('games').insert({
+          user_id: 1,
+          date_time: 990108,
+          location: 'Vivian St',
+          team_a_name: 'the ok team',
+          team_b_name: 'the other team',
+          is_complete: false,
+          team_a_score: 4,
+          team_b_score: 2,
+          sport_name: 'coding',
+          time_elapsed: '00:00:00',
+          is_running: false
+        }),
+        knex('games').insert({
+          user_id: 2,
+          date_time: 990108,
+          location: 'Cuba St',
+          team_a_name: 'the great team',
+          team_b_name: 'the others',
+          is_complete: false,
+          team_a_score: 1,
+          team_b_score: 1,
+          sport_name: 'hackysack',
+          time_elapsed: '00:00:00',
+          is_running: false
+        })
       ])
     })
 }
