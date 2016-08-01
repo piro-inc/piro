21b29c75524feb26e5a01f3f17f0cc1b08936286
rich-farr <scarletmanuka@gmail.com>
2016-08-02 11:27:24 +1200

    Do a whole bunch o' stylin'

--- client/components/Game.js
+++ client/components/Game.js
@@ -37,20 +37,22 @@ class Game extends React.Component {
 
         <div id='game-header'>
           <h2 className='sport-name'>{currentGame.game && currentGame.game.sport_name}</h2>
-          {/* <h3 className='division'>Division</h3>  No division in database yet*/}
+          <h3 className='division'>Division</h3>  {/*No division in database yet*/}
           <h3 className='date-time'>{date}</h3>
           <h3 className='match-location'>{currentGame.game && currentGame.game.location}</h3>
         </div>
 
         <div className='game-score-wrapper'>
           <div className='game-score-card'>
-            <a href='#'><img src='http://placehold.it/60x60.jpg' className='team-logo' /></a>
+            <img src='http://placehold.it/60x60' className='team-logo' />
             <h2 className='team-one'>{currentGame.game && currentGame.game.team_a_name}</h2>
             <h1 className='game-score'>{currentGame.game && currentGame.game.team_a_score}</h1>
           </div>
 
+          <h1 className="period">v</h1>
+
           <div className='game-score-card'>
-            <a href='#'><img src='http://placehold.it/60x60.jpg' className='team-logo' /></a>
+            <img src='http://placehold.it/60x60' className='team-logo' />
             <h2 className='team-two'>{currentGame.game && currentGame.game.team_b_name}</h2>
             <h1 className='game-score'>{currentGame.game && currentGame.game.team_b_score}</h1>
           </div>
