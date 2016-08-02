0be9a811af9ee6e81324f3268b8f17a45a3256c1
Rena Goldsmith <rena.kg@gmail.com>
2016-08-02 18:16:22 +1200

    import menu icon and position dropdown

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -36,21 +36,21 @@ class Navbar extends React.Component {
               <p className='nav-login-register'>register</p>
             </Link>
             : <div className='nav-links'>
-              <p className='nav-login-register'>{user.username}</p>
-              <Link to='/games/new' id='nav-menu' className='drop-down-menu'>+</Link>
-            </div>
-          }
-          <div className='menu-button'>
-            <IconButton name='+' id='demo-menu-lower-right' />
-          </div>
-        </div>
+                <p className='nav-login-register'>{user.username}</p>
 
-        <div className='menu-dropdown'>
-          <Menu target='demo-menu-lower-right' align='right'>
-            <MenuItem onClick={() => browserHistory.push('/')}>Home</MenuItem>
-            <MenuItem onClick={() => browserHistory.push('/games/new')}>Create Game</MenuItem>
-            <MenuItem onClick={this.logout}>Logout</MenuItem>
-          </Menu>
+                <div className='menu-button'>
+                  <IconButton name='more_vert' id='demo-menu-lower-right' />
+                </div>
+
+                <div className='menu-dropdown'>
+                  <Menu target='demo-menu-lower-right' align='right'>
+                    <MenuItem onClick={() => browserHistory.push('/')}>Home</MenuItem>
+                    <MenuItem onClick={() => browserHistory.push('/games/new')}>Create Game</MenuItem>
+                    <MenuItem onClick={this.logout}>Logout</MenuItem>
+                  </Menu>
+                </div>
+              </div>
+          }
         </div>
       </div>
     )
