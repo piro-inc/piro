abbabc64bed244ab50f36c3ea944841465eb6f5d
Marcus Crane <marcus@thingsima.de>
2016-08-03 16:46:52 +1200

    Test for checking if the info page appears

--- client/test/integration/homepage.js
+++ client/test/integration/homepage.js
@@ -31,5 +31,23 @@ module.exports = {
       .pause(1000)
       .assert.urlEquals('http://localhost:3000/games')
       .end()
+  },
+
+  'Info button renders a modal' : function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .waitForElementVisible('body', 5000)
+      .click('button[class=about-icon]')
+      .pause(500)
+      .assert.elementPresent('.skylight-dialog', 'Welcome to PIRO')
+  },
+
+  'Log in form redirects successfully' : function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .waitForElementVisible('body', 5000)
+      .click('button[id=login]')
+      .pause(1000)
+      .assert.elementPresent('#login-form')
   }
 }
