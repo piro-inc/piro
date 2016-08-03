a0ed677fe08ed3adca646474e654de1f3714ffea
danielkermode <danielkermode@hotmail.co.nz>
2016-08-03 18:36:33 +1200

    lint

--- client/components/Egg.js
+++ client/components/Egg.js
@@ -3,7 +3,7 @@ import React from 'react'
 export class Egg extends React.Component {
   render () {
     return (
-    <iframe width="420" height="315" src="https://www.youtube.com/embed/TazHNpt6OTo" frameborder="0" allowfullscreen></iframe>
+      <iframe width='420' height='315' src='https://www.youtube.com/embed/TazHNpt6OTo' frameborder='0' allowfullscreen></iframe>
     )
   }
 }
